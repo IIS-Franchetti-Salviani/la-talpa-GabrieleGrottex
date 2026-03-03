401008446b8671a3df9867fa100aa9ef61cd0b76
grottelli-gabriele <grottelli.gabriele@franchettisalviani.net>
2026-03-03 15:59:33 +0100

    creazione gestore del gioco
    inserimento degli import, attributi e il costruttore

--- GiocoTalpa/src/giocotalpa/GestoreGioco.java
+++ GiocoTalpa/src/giocotalpa/GestoreGioco.java
@@ -4,11 +4,26 @@
  */
 package giocotalpa;
 
-
+import java.util.Random;
+import javax.swing.SwingUtilities;
 /**
  *
  * @author grottelli.gabriele
  */
 public class GestoreGioco {
     
+    private Buca[] buche;
+    private Giocatore giocatore;
+    private InterfacciaGrafica gui;
+    private boolean attivo = true;
+    private Random random = new Random();
+
+    public GestoreGioco(InterfacciaGrafica gui){
+        this.gui = gui;
+        this.giocatore = new Giocatore("Gabriele");
+        buche = new Buca[8];
+        for(int i=0; i<8; i++){
+            buche[i] = new Buca(i);
+        }
+    }
 }
