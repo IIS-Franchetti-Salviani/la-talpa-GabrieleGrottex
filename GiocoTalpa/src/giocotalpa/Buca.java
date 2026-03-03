6987d619dca92f1419f73e4ff2f394bf7bf269da
grottelli-gabriele <grottelli.gabriele@franchettisalviani.net>
2026-03-03 15:57:42 +0100

    creazione classe buca
    creazione della classe per le buche da far comparire

--- GiocoTalpa/src/giocotalpa/Buca.java
+++ GiocoTalpa/src/giocotalpa/Buca.java
@@ -9,5 +9,43 @@ package giocotalpa;
  * @author grottelli.gabriele
  */
 public class Buca {
-    
+    private int id;
+    private Talpa talpaCorrente;
+    private boolean occupata;
+
+    public Buca(int id) {
+        this.id = id;
+        this.occupata = false;
+    }
+
+    public synchronized void mostraTalpa(Talpa t) {
+        talpaCorrente = t;
+        t.appare();
+        occupata = true;
+    }
+
+    public synchronized void nascondiTalpa() {
+        if(talpaCorrente != null){
+            talpaCorrente.scompare();
+        }
+        occupata = false;
+        talpaCorrente = null;
+    }
+
+    public synchronized int colpisci() {
+        if(occupata && talpaCorrente.eAttiva()){
+            int punti = talpaCorrente.getPunteggio();
+            nascondiTalpa();
+            return punti;
+        }
+        return -5; // penalità se colpisci buca vuota
+    }
+
+    public boolean eOccupata() {
+        return occupata;
+    }
+
+    public int getId() {
+        return id;
+    }
 }
