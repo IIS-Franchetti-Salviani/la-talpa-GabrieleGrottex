18795ebb567d54b2ef0a2b51bcc49da173000dab
grottelli-gabriele <grottelli.gabriele@franchettisalviani.net>
2026-03-03 15:56:57 +0100

    creazione classe giocatore
    il commit precedente era sbagliato era la classe talpa che ho implementato mi sono confuso sui nomi

--- GiocoTalpa/src/giocotalpa/Giocatore.java
+++ GiocoTalpa/src/giocotalpa/Giocatore.java
@@ -9,5 +9,23 @@ package giocotalpa;
  * @author grottelli.gabriele
  */
 public class Giocatore {
-    
+    private String nome;
+    private int punteggio;
+
+    public Giocatore(String nome){
+        this.nome = nome;
+        this.punteggio = 0;
+    }
+
+    public void aggiungiPunti(int punti){
+        punteggio += punti;
+    }
+
+    public int getPunteggio(){
+        return punteggio;
+    }
+
+    public String getNome(){
+        return nome;
+    }
 }
