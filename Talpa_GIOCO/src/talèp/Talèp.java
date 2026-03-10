ea7f787dcfb6f8233dc4525f812be8faf6558ceb
grottelli-gabriele <grottelli.gabriele@franchettisalviani.net>
2026-03-10 10:32:14 +0100

    ho sbagliato file
    ricreazione del codice

---
+++ Talpa_GIOCO/src/talèp/Talèp.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package talèp;
+
+/**
+ *
+ * @author grottelli.gabriele
+ */
+public class Talèp {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
